6b953a1a3989c80f37643d58f751e64946e1f32f
kimpangya <nam74415133@gmail.com>
2024-01-12 09:22:38 +0900

    test21

---
+++ src/JavaAlgorithm/test21.java
@@ -0,0 +1,19 @@
+package JavaAlgorithm;
+
+public class test21 {
+    public boolean solution(int x) {
+        boolean answer = false;
+        int originX=x;
+        int rest;
+        int xSum=0;
+        for(int i=10000;i>0;i/=10){
+            rest = x%i;
+            xSum+=x/i;
+            x=rest;
+        }
+        if(originX%xSum==0){
+            answer=true;
+        }
+        return answer;
+    }
+}
